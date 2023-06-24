db2abb9f0ff700957ad6adfb6dcf1f0f51c32c34
huski-commando <gyan.mu@gmail.com>
2023-06-24 23:57:05 +0530

    changed logo

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import { loader as singleCocktailLoader } from "./pages/Cocktail";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5,
+      staleTime: 1000 * 60 * 10,
     },
   },
 });
